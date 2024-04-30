89ef25aacf00ff9f20e3d3ba2f89a6187abe054d
Gaurav Singh Negi <gnegi897@gmail.com>
2024-04-30 19:19:29 +0530

    adding loading spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,20 @@
+
+import ClipLoader from 'react-spinners/ClipLoader';
+
+const override = {
+    display: 'block',
+    margin:'100px auto'
+}
+
+const Spinner = ({loading}) => {
+  return (
+    <ClipLoader
+    loading={loading}
+    color='#4338ca'
+    cssOverride={override}
+    size={150}
+    />
+  )
+}
+
+export default Spinner
--- src/components/joblistings.jsx
+++ src/components/joblistings.jsx
@@ -1,54 +1,47 @@
-import React from 'react'
-import { useState , useEffect } from 'react'
-import Jobs from '../jobs.json'
-import Joblisting from './joblisting'
+import React from "react";
+import { useState, useEffect } from "react";
+import Jobs from "../jobs.json";
+import Joblisting from "./joblisting";
+import Spinner from "./Spinner";
+const Joblistings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const Joblistings = ({ isHome = false}) => {
-
-  const [jobs , setJobs] = useState([]);
-  const [loading , setLoading] = useState(true);
-
-  useEffect(()=>{
-      const fetchjobs = async () =>{
-        try{
-          const res = await fetch('http://localhost:8000/jobs');
-          const data = await res.json();
-          setJobs(data);
-        }
-
-        catch(error)
-        {
-          console.log('Error fetching data',error);
-        }
-        finally{
-          setLoading(false);
-        }
+  useEffect(() => {
+    const fetchjobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
       }
+    };
 
-      fetchjobs();
-  },[])
+    fetchjobs();
+  }, []);
   return (
     <section className="bg-blue-50 px-4 py-10">
-    <div className="container-xl lg:container m-auto">
-      <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        {isHome ? "New Jobs" : "Browse Jobs"}
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {loading ? (<h2>Loading....</h2>) : 
-       (
-        <>
-        {jobs.map((Job) => (
-          <Joblisting key={Job} Job ={Job}/>
-        ))
-      }
-      </>
-    )
-    }
-    
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "New Jobs" : "Browse Jobs"}
+        </h2>
+          {loading ? (
+            <Spinner loading={loading}/>
+          ) : (
+            <>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((Job) => (
+                <Joblisting key={Job} Job={Job} />
+              ))}
+        </div>
+            </>
+          )}
       </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Joblistings
+export default Joblistings;
